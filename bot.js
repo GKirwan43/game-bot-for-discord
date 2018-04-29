1e5d714f4007bdb514bb2fca9c00b9b772a38766
GKirwan43 <38820290+GKirwan43@users.noreply.github.com>
2018-04-29 12:05:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ bot.on("message", async message => {
 
   if (cmd === `${prefix}playgame roll dice`){
     message.channel.send("Rolling Dice... You rolled a 5!");
-  }else if (cmd === "${prefix}playgame"){
+  }else if (cmd === `${prefix}playgame`){
     message.channel.send("Select a game");
   }
 });
