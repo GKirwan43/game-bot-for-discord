5f9665c124e708e265ad31c4d48f781f3e02756f
GKirwan43 <38820290+GKirwan43@users.noreply.github.com>
2018-04-29 11:42:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,8 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
+  if (cmd === `${prefix}hello`){
+    message.channel.send("Hello!");
   }
 });
 
